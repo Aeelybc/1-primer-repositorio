2989a9148a29f09ab64ca332f2b1eeb73a8151d3
Aeelybc <areli.bc3@gmail.com>
2024-07-10 18:49:00 -0700

    hola

---
+++ storage.js
@@ -0,0 +1,18 @@
+(function)({
+    //todo lo que hay aqui
+    let form = document.querySelector( 'form' ),
+        list = document.querySelector( '#list' ),
+        item = document.querySelector ( 'item' )
+   
+        form.addEventlistener( 'subit', function (e)){
+            list.innerHTML+= '<li' +item.value + '</li>'
+    
+        }, false)
+
+        Function StorageEvent() {
+            localStorage.myitems = list. innerHTML
+
+        }
+        
+        
+})()
